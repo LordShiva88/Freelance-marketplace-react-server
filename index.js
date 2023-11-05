a65a6b4afef0bee8bea0b9124e2dc26aa7238c22
LordShiva <litonnath4888@gmail.com>
2023-11-05 22:26:27 +0600

    Get Data by filtering

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // Middle Ware
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res)=>{
-  res.send('Hello My server is running')
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rqtbidh.mongodb.net/?retryWrites=true&w=majority`;
 
-app.listen(port, ()=>{
-  console.log(`My server is running with port ${port}`)
-})
+console.log(process.env.DB_USER);
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const jobCollection = client.db("FreelanceBD").collection("Jobs");
+
+    app.get("/jobs", async (req, res) => {
+      let query = {};
+      const { category } = req.query;
+      if (category) {
+        query = { category: category };
+      }
+      const result = await jobCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello My server is running");
+});
+
+app.listen(port, () => {
+  console.log(`My server is running with port ${port}`);
+});
